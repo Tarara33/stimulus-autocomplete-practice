68d0bdafeea2ab5981ba575f3e5de05832a4a9bd
Sarina <sarina0618@gmail.com>
2024-01-13 20:55:00 +0900

    ✨ :オートコンプリート実装

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import { Autocomplete } from 'stimulus-autocomplete'  //コンポーネントを読み込むための記述
 
 const application = Application.start()
+application.register('autocomplete', Autocomplete)  //コンポーネントにあるAutocompleteコントローラを使えるようにするための記述
 
 // Configure Stimulus development experience
 application.debug = false
